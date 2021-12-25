faed1794d670e586c5aef3b8862ca3c3a2df7fb3
lucas.simao <lucas.simao@mutant.com.br>
2021-12-25 18:27:02 -0300

    test: ensure trigger emission when called

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -2,40 +2,53 @@ import { TestBed } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIDService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+
 import { LikeWidgetModule } from './like-widget.module';
 const makeSut = async () => {
   await TestBed.configureTestingModule({
     imports: [LikeWidgetModule],
   }).compileComponents();
-  const component = TestBed.createComponent(LikeWidgetComponent);
-  const sut = component.componentInstance;
-  return { sut, component };
+  const fixture = TestBed.createComponent(LikeWidgetComponent);
+  const sut = fixture.componentInstance;
+  return { sut, fixture };
 };
 
 describe(LikeWidgetComponent.name, () => {
-  it('should create component', async () => {
+  it('should create fixture', async () => {
     const { sut } = await makeSut();
 
     expect(sut).toBeTruthy();
   });
 
   it('should auto generate ID when id input property is missing', async () => {
-    const { sut, component } = await makeSut();
+    const { sut, fixture } = await makeSut();
 
-    component.detectChanges();
+    fixture.detectChanges();
 
     expect(sut.id).toBeTruthy();
   });
 
   it('should not generate ID when id input property is present', async () => {
-    const { sut, component } = await makeSut();
+    const { sut, fixture } = await makeSut();
     const fakeId = 'any_id';
 
     sut.id = fakeId;
-    component.detectChanges();
+    fixture.detectChanges();
 
-    const result = component.componentInstance.id;
+    const result = fixture.componentInstance.id;
 
     expect(result).toBe('any_id');
+  });
+
+  describe(LikeWidgetComponent.prototype.like.name, () => {
+    it('should trigger emission when called', async () => {
+      const { sut, fixture } = await makeSut();
+      fixture.detectChanges();
+      const emitSpy = spyOn(sut.liked, 'emit');
+
+      sut.like();
+
+      expect(emitSpy).toHaveBeenCalled();
+    });
   });
 });
