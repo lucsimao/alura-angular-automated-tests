67e09da9ccec066fdbe7a1b9f365e7b3e7a5f982
lucas.simao <lucas.simao@mutant.com.br>
2021-12-25 17:18:01 -0300

    refactor: using another approach for TestBed

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -2,11 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIDService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetModule } from './like-widget.module';
 const makeSut = async () => {
   await TestBed.configureTestingModule({
-    declarations: [LikeWidgetComponent],
-    providers: [UniqueIDService],
-    imports: [FontAwesomeModule],
+    imports: [LikeWidgetModule],
   }).compileComponents();
   const sut = TestBed.createComponent(LikeWidgetComponent);
 
