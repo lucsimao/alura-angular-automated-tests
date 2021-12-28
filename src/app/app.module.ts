69a08cf65b2b9d48e48ca64fdced7f4a505caa04
Lucas Simão <lucas.simao@mutant.com.br>
2021-12-27 22:08:51 -0300

    feat: add like event to photo-frame component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { NgModule } from '@angular/core';
+import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LikeWidgetModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LikeWidgetModule,
+    PhotoFrameModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {}
+export class PhotoFrameComponent {
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+  @Input() public description = '';
+  @Input() public src = '';
+  @Input() public likes = 0;
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
