6760ca8282eacf4eb2fff7297e64a7b8bca2870d
lucsimao <lucasimao64@gmail.com>
2021-12-29 19:24:41 -0300

    feat: create appAction directive

--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -1,3 +1,4 @@
+import { ActionDirectiveModule } from './../../directives/action/action.module';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LikeWidgetComponent } from './like-widget.component';
@@ -6,7 +7,7 @@ import { UniqueIDService } from '../../services/unique-id/unique-id.service';
 
 @NgModule({
   declarations: [LikeWidgetComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, ActionDirectiveModule],
   exports: [LikeWidgetComponent],
   providers: [UniqueIDService],
 })
