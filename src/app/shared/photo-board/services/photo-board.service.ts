298b66e206ddda8c92a481c69bb408d42a69764b
lucsimao <lucasimao64@gmail.com>
2021-12-29 23:35:36 -0300

    feat: converting photos description to uppercase

--- src/app/shared/photo-board/services/photo-board.service.ts
+++ src/app/shared/photo-board/services/photo-board.service.ts
@@ -1,8 +1,9 @@
+import { delay, map, tap } from 'rxjs/operators';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Photo } from './../interfaces/photo';
-import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoBoardService {
@@ -11,6 +12,15 @@ export class PhotoBoardService {
   public getPhotos(): Observable<Photo[]> {
     return this.httpClient
       .get<Photo[]>('http://localhost:3000/photos')
+      .pipe(
+        map((photos) => {
+          return photos.map((photo) => ({
+            ...photo,
+            description: photo.description.toUpperCase(),
+          }));
+        })
+      )
+      .pipe(tap((photos) => console.log(photos)))
       .pipe(delay(2000));
   }
 }
