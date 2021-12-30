1b2bb4f7cb4251a5084f66ca7adaf40b6f5c6b2d
lucsimao <lucasimao64@gmail.com>
2021-12-29 23:28:07 -0300

    test: ensure display board when data arrives

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,16 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
+import { Photo } from './../../shared/photo-board/interfaces/photo';
+import { PhotoBoardService } from './../../shared/photo-board/services/photo-board.service';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+const makePhotoList = (): Photo[] => {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: '',
+    });
+  }
+  return photos;
+};
+
 const makeSut = async () => {
   await TestBed.configureTestingModule({
     imports: [PhotoListModule, HttpClientModule],
   }).compileComponents();
 
   const fixture = TestBed.createComponent(PhotoListComponent);
+  const service = TestBed.inject(PhotoBoardService);
   const sut = fixture.componentInstance;
 
-  return { sut };
+  return { sut, fixture, service };
 };
 
 describe(PhotoListComponent.name, () => {
@@ -18,5 +35,20 @@ describe(PhotoListComponent.name, () => {
     const { sut } = await makeSut();
 
     expect(sut).toBeTruthy();
+  });
+
+  describe('Dom', () => {
+    it('Should display board when data arrives', async () => {
+      const { fixture, service } = await makeSut();
+
+      const photos = makePhotoList();
+      spyOn(service, 'getPhotos').and.returnValue(of(photos));
+      fixture.detectChanges();
+      const board = fixture.nativeElement.querySelector('app-photo-board');
+      const loader = fixture.nativeElement.querySelector('.loader');
+
+      expect(board).not.toBeNull();
+      expect(loader).toBeNull();
+    });
   });
 });
