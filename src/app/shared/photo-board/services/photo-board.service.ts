eb4748a781f5d4d609b5e404c7a8f8ab1c80a71c
lucsimao <lucasimao64@gmail.com>
2021-12-29 22:34:46 -0300

    feat: add proposital delay to simulate load

--- src/app/shared/photo-board/services/photo-board.service.ts
+++ src/app/shared/photo-board/services/photo-board.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Photo } from './../interfaces/photo';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoBoardService {
   constructor(private httpClient: HttpClient) {}
 
   public getPhotos(): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>('http://localhost:3000/photos');
+    return this.httpClient
+      .get<Photo[]>('http://localhost:3000/photos')
+      .pipe(delay(2000));
   }
 }
